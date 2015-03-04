33b53ac03af049ef5e23dd31a8f1617e2f4ddd37
Oscar <oscar.medina101@alu.ulpgc.es>
2015-03-04 13:54:05 +0000

    prueba en activity

--- src/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+//prueba a ver si va
 
 public class QuizActivity extends Activity {
 
